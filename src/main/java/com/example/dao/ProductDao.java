fba5c1c934b4a5ffa8fca21e616f92012f114194
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2021-05-25 17:03:30 +0530

    Comiting changes for Repo

---
+++ src/main/java/com/example/dao/ProductDao.java
@@ -0,0 +1,14 @@
+package com.example.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+
+	
+	// Emergancy HIS_134 stroy submitting now
+	
+	public void displayHis_134() {
+		
+	}
+}
--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -1,5 +1,13 @@
 package com.example.demo.models;
 
 public class Product {
+	
+	// Working on HIS-122 which is in betwween stroy
+	
+	public void display() {
+		
+		System.out.println("Welcome HIS-122");
+		
+	}
 
 }
