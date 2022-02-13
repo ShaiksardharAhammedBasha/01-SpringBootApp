9591e75d4233947a939051f833130696b61f87c6
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-02-13 19:31:01 +0530

    removed prodcut description

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -1,21 +1,11 @@
 package com.example.demo.models;
 
 public class Product {
-	
-	private String productDesc = "In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand; it is anything that can be offered to a market to satisfy the desire or need of a customer.[1] In retailing, products are often referred to as merchandise, and in manufacturing, products are bought as raw materials and then sold as finished goods. A service is also regarded as a type of product.\n" +
-			"\n" +
-			"Commodities are usually raw materials such as metals and agricultural products, but a commodity can also be anything widely available in the open market. In project management, products are the formal definition of the project deliverables that make up or contribute to delivering the objectives of the project.\n" +
-			"\n" +
-			"A related concept is that of a sub-product, a secondary but useful result of a production process.\n" +
-			"\n" +
-			"Dangerous products, particularly physical ones, that cause injuries to consumers or bystanders may be subject to product liability.";
 	private int p1id;
 	private String p1name;
 	
 	public void display() {
-		
 		System.out.println("Welcome HIS-122");
-		
 	}
 
 }
