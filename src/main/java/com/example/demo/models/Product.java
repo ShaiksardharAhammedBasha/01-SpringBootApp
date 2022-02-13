c8769bec89f1c16fc67359d564c17021fbf8d64c
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-02-13 19:28:02 +0530

    Revert "Revert "Pushing To Master Branch With New Method DisplayProduct""
    This reverts commit 82a1eedbea2214928387316739e094c572289566.

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -1,7 +1,11 @@
 package com.example.demo.models;
 
 public class Product {
-	
+
+	private int pid;
+	private String pname;
+
+
 	private String productDesc = "In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand; it is anything that can be offered to a market to satisfy the desire or need of a customer.[1] In retailing, products are often referred to as merchandise, and in manufacturing, products are bought as raw materials and then sold as finished goods. A service is also regarded as a type of product.\n" +
 			"\n" +
 			"Commodities are usually raw materials such as metals and agricultural products, but a commodity can also be anything widely available in the open market. In project management, products are the formal definition of the project deliverables that make up or contribute to delivering the objectives of the project.\n" +
@@ -11,7 +15,7 @@ public class Product {
 			"Dangerous products, particularly physical ones, that cause injuries to consumers or bystanders may be subject to product liability.";
 	private int p1id;
 	private String p1name;
-	
+
 	public void display() {
 		
 		System.out.println("Welcome HIS-122");
