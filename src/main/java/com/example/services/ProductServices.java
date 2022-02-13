408c35bbd9a716fc7b2598158cc26a11f927e0c1
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-02-13 14:28:36 +0530

    added git ignore and product services

--- src/main/java/com/example/services/ProductServices.java
+++ src/main/java/com/example/services/ProductServices.java
@@ -1,8 +1,16 @@
 package com.example.services;
 
+import com.example.dao.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class ProductServices {
+
+    @Autowired
+    private ProductDao dao;
+
+    
 
 }
